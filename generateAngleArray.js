69717e994fdcb71b73be1fd8113ce0844100c3f4
David Young <tgsstdio@gmail.com>
2019-12-03 10:46:03 +1100

    generate angle per index  as separate fn

--- generateAngleArray.js
+++ generateAngleArray.js
@@ -1,13 +1,5 @@
 const generateGuidelineVector = require('./generateGuideVector')
 
-function clampDownToZero(value) {
-  if (Math.abs(value) <= Number.EPSILON) {
-    return 0.0
-  } else {
-    return value;
-  }
-}
-
 // ASSUME: from 12 o'clock going anti-clockwise
 function generateAngleArray(noOfElements) {
   let angleArray = []
