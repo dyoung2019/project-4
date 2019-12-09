a14089d845e51bdca53bf5998f2310a911b0c02f
David Young <tgsstdio@gmail.com>
2019-12-09 22:33:49 +1100

    seg.selected to see handles

--- prototypes/qs6/src/App.js
+++ prototypes/qs6/src/App.js
@@ -6,10 +6,6 @@ import './App.css';
 const loadPaper = (pScope) => {
   // Create an empty project and a view for the canvas:
 
-  pScope.view.on('mousedown', function(event) {
-    console.log('mouse down :' + event.point);
-  })
-
   // Create a Paper.js Path to draw a line into it:
   var path = new pScope.Path();
   path.strokeColor = 'black';
@@ -23,12 +19,15 @@ const loadPaper = (pScope) => {
   path.fullySelected = true;
   
   // Create a copy of the path and move it 100pt to the right:
-  var copy = path.clone();
+  // var copy = path.clone();
+  var copy = new pScope.Path()
+  // copy.add(new pScope.Point(130, 75)); 
+  // copy.add(new pScope.Point(130, 25)); 
   copy.fullySelected = true;
-  copy.position.x += 100;
+  // copy.position.x += 100;
   
   // Smooth the segments of the copy:
-  copy.smooth();
+  // copy.smooth();
 
   var tool = new pScope.Tool();
   // var scribble = new pScope.Path();
@@ -45,35 +44,61 @@ const loadPaper = (pScope) => {
   //   console.log('angle :' + angle);
   // }
 
-  var endPoint = copy.segments[0]
+  // var endPoint = copy.segments[0]
+  var endPoint = null
   
+  function pullHandles(segment, point) {
+    let dx = segment.point.x - point.x
+    let dy = segment.point.y - point.y
+
+    endPoint.handleIn = new pScope.Point(dx, dy)
+    endPoint.handleOut = new pScope.Point(-dx, -dy)
+  }
+
+  let isExtendingHandles = false
   // scribble.add(center);
   // Define a mousedown and mousedrag handler
   tool.onMouseDown = function(event) {
-    endPoint.handleIn = event.point
+    // endPoint.handleIn = event.point
+    //pullHandles(endPoint, event.point)
+    // if (!isExtendingHandles) {
+    //   endPoint = copy.add(event.point)
+    // }
 
     // printInfo(event.point)
-  }
+    // let noOfSegments = copy.segments.length
+    // console.log(noOfSegments)
+    // copy.add(event.point)
 
-  function editHandleDirectly(event, segment) {
-    let dx = event.point.x - segment.point.x
-    let dy = event.point.y - segment.point.y
+    // endPoint = copy.segments[0]
+    if (endPoint == null) {
+      let seg = copy.add(event.point)
+      // console.log("Array : " + seg)
+      seg.selected = true;
+      endPoint = seg
+    }
 
-    return new pScope.Point(dx, dy)
-  }
-
-  function editHandleIndirect(event, segment) {
-    let dx = segment.point.x - event.point.x
-    let dy = segment.point.y - event.point.y
-
-    return new pScope.Point(dx, dy)
+    isExtendingHandles = true
+    // console.log('mouse down')
+    return false
   }
 
   tool.onMouseDrag = function(event) {
-    endPoint.handleOut = editHandleDirectly(event, endPoint)
-    endPoint.handleIn = editHandleIndirect(event, endPoint)
-    console.log('mouse drag :' + event.point);
+    if (!!endPoint) {
+      // if (copy.segments.length === 1) {
+        pullHandles(endPoint, event.point)
+      // }
+    // endPoint.handleOut = editHandleDirectly(event, endPoint)
+    // endPoint.handleIn = editHandleIndirect(event, endPoint)
+    // console.log('mouse drag :' + event.point);
     // printInfo(event.point)
+    }
+  }
+
+  tool.onMouseUp = function(event) {
+    console.log('mouse up :' + event.point);
+    isExtendingHandles = false
+    endPoint = null
   }
 
   tool.onKeyDown = function(event) {
