afa9d7ee306ffec7174f85b7892f3b7312714aca
David Young <tgsstdio@gmail.com>
2019-12-16 08:57:34 +1100

    last changes on prototypes

---
+++ docs/pseudo-code.md
@@ -0,0 +1,111 @@
+# Psuedo-code
+
+## Idea #1
+
+Implementation 
+ - a single instance of p5.js context
+ - multiple paper.js instance per layer
+ - one visible canvas (p5.createCanvas) on screen 
+ - one off-screen canvas (p5.createGraphics) for mask per layer
+ - two p5 images (mask, source) per layer (not quite)
+
+Vector mask
+- One off-screen canvas (p5.createGraphics)
+- One paper.js paper scope and layer 
+
+Adjustment layer 
+- One image of all layers below the current layer (p5.createGraphics) 
+- One p5 off-screen canvas (p5.createGraphics) for vector mask
+
+Image layer 
+- One image loaded from jpg, png
+- One layer for vector mask
+
+## Idea #2
+### HTML 
+- Sources
+  - p5 canvas are created here
+  - img tags are created here too
+- Masks
+  - canvas as vector masks (p5 offscreen canvas)
+
+## Idea #3
+### HTML 
+- Workspace
+  - Composition(s)
+    - Layer(s) attribute
+      - Source
+      - Mask
+
+## Idea #4 
+- p5 main container
+  - separate sketch
+  - background color
+  - react
+    - layer
+      - source canvas div
+      - mask canvas div
+
+- React states
+  - p5 instances
+    - loop
+    - setup
+      - no loop
+      - redraw on change  
+
+- Composition
+  - layers
+    - Thumbnail(s)?
+      - p5 sketch canvas (separate resolution)
+        - setup
+          - canvas (image size)
+          - no loop 
+    - Source div
+      - Canvas
+    - Mask div
+      - Canvas
+    
+
+
+## Layers
+### New Layer 
+
+1. Click on add button
+1. To create a new layer, we need a 
+    1. Source 
+      1. If solid color required, then call p5.createGraphics 
+      1. If 
+    1. Mask
+      Create a p5 graphics object for mask
+    -  with class = paper canvas
+  1. Append HTML id, attribute
+  - with opacity = 100, position = (0, 0), rotation = 0, scale = 100%, 100%
+1. Add to layer list above the highlighted layer 
+1. Change highlight layer (focus)
+
+### Delete Layer
+
+1. Delete Button
+1. Delete the highlighted layer (focus)
+1. Remove layer from list
+
+### Thumbnails
+1. Get layer info for state
+1. Requires p5 instance
+1. Pass canvas id + mask id
+1. Create a tiny div
+
+
+### Undo History 
+
+
+Based on Redux
+````js
+
+history {
+  past: [1, 2, 3]
+  current: 4
+  future:[5, 6]
+}
+
+````
--- prototypes/mixed-canvas/app.js
+++ prototypes/mixed-canvas/app.js
@@ -1,5 +1,4 @@
 const paperContainer = document.querySelector('.paper-container')
-const paperCanvas = document.querySelector('.paper-container .paper-canvas')
 const p5Canvas = document.querySelector('.p5-container .p5-canvas')
 
 // Get a reference to the canvas object
@@ -27,27 +26,11 @@ var loadPaper = (canvas) => {
   
   // Smooth the segments of the copy:
   copy.smooth();
+
+  console.log('done')
 }
 
-loadPaper(paperCanvas)
-
-
-let handleComposition = p => {
-  p.setup = () => {
-    let w = 400
-    let h = 400
-
-    p.createCanvas(w, h);
-    p.background(220);
-  }
-
-  p.draw = () => {
-    p.background(220)
-  }
-}
-
-
-let sketch = (p, options) => {
+let sketch = (p) => {
   var layer_1;
   var layer_0;
 
@@ -66,16 +49,27 @@ let sketch = (p, options) => {
   } 
 
   p.setup = () => {
-    let w = 400
-    let h = 400
+    let w = 150
+    let h = 150
 
     p.createCanvas(w, h);
-    p.background(220);
+    // p.background(220);
 
     // create a p5.Graphics containing the image that will be masked
     layer_1 = p.createGraphics(w,h);
     layer_1.elt.id = "layer_1"
-    layer_1.parent(paperContainer);
+    // layer_1.elt.('resize');
+    paperContainer.style.width = w
+    paperContainer.style.height = h
+
+    layer_1.attribute('resize', 'true')
+    layer_1.attribute('hidpi', 'true')
+    layer_1.addClass('paper-canvas')
+    layer_1.parent(paperContainer)
+
+    // const paperCanvas = document.querySelector('.paper-container .paper-canvas')
+    loadPaper(layer_1.elt)
+
     layer_0 = generateBackground(w, h)
     layer_0.elt.id = "layer_0"
   }
@@ -83,20 +77,21 @@ let sketch = (p, options) => {
   p.draw = () => {
     // p.clear()
     // STEP 0: clear background color on canvas
-    p.background(220)
+  //  p.background(220)
     
     // overlay
     // p.background(0);
 
     let opacity = 65
     // STEP 1: layer 1 must be clear
-    layer_1.clear()
+    // layer_1.clear()
     // p.background(0);
     // STEP 2: COLOR IS SET ON p5 instance
     p.fill(255, 255, 255, opacity);
     // STEP 3: Draw here on layer
-    layer_1.ellipse(p.mouseX, p.mouseY, 250);
+    //layer_1.ellipse(p.mouseX, p.mouseY, 250);
 
+    /*
     // let mergeLayers = composeImages(layer_0, layer_1)
     // STEP 4A: create sub image of src image
     let subImage = layer_0.get(0, 0, layer_0.width, layer_0.height)
@@ -108,11 +103,13 @@ let sketch = (p, options) => {
     subImage.mask(subMask)
     // debugger
     // p.image(subImage, 0, 0)
+    */
+    p.image(layer_1, 0, 0)
     // STEP 6: apply layer's blend mode
-    let blendMode = p.BLEND
-    p.blendMode(blendMode)
+    //let blendMode = p.BLEND
+    //p.blendMode(blendMode)
     // STEP 7: draw main canvas
-    p.image(subImage, 0, 0)
+   // p.image(subImage, 0, 0)
     // STEP 8: reset blend mode
     // p.blendMode(p.BLEND)
   };
@@ -120,6 +117,8 @@ let sketch = (p, options) => {
 
 
 let myp5 = new p5(sketch, p5Canvas);
+
+// const paperCanvas = document.querySelector('.paper-container .paper-canvas')
 // loadPaper(paperCanvas)
 
 
